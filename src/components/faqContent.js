383175a4695b4d15fcdae11f31ca31282ce3c658
ajay jairath <gjairath@purdue.edu>
2021-10-22 23:34:11 -0400

    Faq page complete

--- src/components/faqContent.js
+++ src/components/faqContent.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 import styled from 'styled-components'
-import styles from './styles/faq.module.css'; 
 
-import {MdExpandMore} from 'react-icons/md'
+import Faq from "react-faq-component";
+import "./styles/faq.css"
+
+const data = {
+    title: "General Questions",
+    rows: [
+        {
+            title: "How do I use this application?",
+            content: `Read the About Page.`,
+        },
+        {
+            title: "I am a Discord Moderator/Admin, I think this application is dumb, I want to complain.",
+            content:
+                "You can contact me at get-a-life@virgin.you",
+        },
+        {
+            title: "I am a Polylinguist with a passion for learning languages, I have genuine useful feedback.",
+            content: `Great! Contact me at: lang-learner-us@gmail.com`,
+        },
+        {
+            title: "I enjoy high-quality memes, any suggestions?",
+            content: `Always. Reddit has some pretty good stuff but honestly, finding exceptional memes is more science than
+                      it is art. It takes alot of skill to do this right. r/dankmemes is great if you're into some PG-rated memes.
+                      r/terriblefacebookmemes is my personal favorite. I intend on writing several blog posts about this so I
+                      will definitely post it here as soon as I can.`,
+        },
+        {
+            title: "How I cash in my winnings?",
+            content: `Congratulations! Follow the instructions <a style="text-decoration: none;" href="https://youtu.be/JMcYVYbDGBU?t=43" target="_blank" rel="noreferrer noopener"> here. </a>`,
+        },
+        {
+            title: "Can't I just use X/Y/Z instead? I mean if I wanted to I could use X/Y/Z instead.",
+            content: `Ok.`,
+        },
+    ],
+};
+
 
 const FaqContent = (props) => {
 
@@ -21,86 +56,19 @@ const FaqContent = (props) => {
 }
 
 
-const OverviewFaq = (props) => {
-    // this is not very scalable, but im not gonna redo it so idc
-    
-     return (
-        <section className={styles.general_questions}>
-            
-            <div className={styles.section_header}>
-                <h1 className={styles.h}> General Questions </h1>
-            </div>
-            
-            <div className={styles.divider}>
-            </div>
-            
-            <div className={styles.section_desc}>
-                <div className={styles.question}>
-                    <div className={styles.question_title}>
-                    How do I use this application?
-                    </div>
-                    
-                    <div className={styles.question_icon}>
-                        <MdExpandMore/>
-                    </div>
-                </div>
-                
-                <div className={styles.question}>
-                    <div className={styles.question_title}>
-                    I am a Discord Moderator/Admin, I think this application is dumb, I want to complain.
-                    </div>
-                    
-                    <div className={styles.question_icon}>
-                        <MdExpandMore/>
-                    </div>
-                </div>
-            
-                <div className={styles.question}>
-                    <div className={styles.question_title}>
-                    I am a Polylinguist with a passion for learning languages, I have genuine useful feedback.
-                    </div>
-                    
-                    <div className={styles.question_icon}>
-                        <MdExpandMore/>
-                    </div>
-                </div>
-                
-                <div className={styles.question}>
-                    <div className={styles.question_title}>
-                    I enjoy high-quality memes, any suggestions?
-                    </div>
-                    
-                    <div className={styles.question_icon}>
-                        <MdExpandMore/>
-                    </div>
-                </div>
-                
-                <div className={styles.question}>
-                    <div className={styles.question_title}>
-                    How I cash in my winnings?
-                    </div>
-                    
-                    <div className={styles.question_icon}>
-                        <MdExpandMore/>
-                    </div>
-                </div>
-                
-                <div className={styles.question}>
-                    <div className={styles.question_title}>
-                    Can't I just use X/Y/Z instead? [ok.]
-                    </div>
-                    
-                    <div className={styles.question_icon}>
-                        <MdExpandMore/>
-                    </div>
-                </div>
-            </div>
-                    
-            
-        </section>
+const OverviewFaq = () => {
 
-  );
-};
+    return (
+        <div>
+            <Faq
+                data={data}
+            />
+            
+                <div style={{height: "50px"}}> </div>
+
+        </div>
+    );
+}
 
 
 const ContainerBelow = styled.div`
@@ -115,7 +83,6 @@ const WrapperBelow = styled.div`
     font-family: "Denso Var", sans-serif;
     display: flex;
     align-items: center;
-
     
     height: 100%;
     width: 69%; /* he he*/
